f76aa80933eb1f33ad0bab79dbd97a629a0193fc
Tara <hymerstara@gmail.com>
2024-02-03 23:26:41 -0700

    program as pairs

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  let result = [];
 
-module.exports = wordSearch
+  //Horizontal search
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // Vertical search
+  letters.forEach((row) => {
+    row.forEach((element, idx) => {
+      //group numbers by index (idx) and put to results
+
+      result[idx] ||= []; // initialize result[idx] as an empty array.
+      //result[idx] is the element of the given array.
+
+      result[idx].push(element); //result is the new array, idx is the index, element is the character. result[idx] = index 0..., pushing all the elements in index 0;
+    });
+  });
+
+  const verticalJoin = result.map((char) => char.join(""));
+
+  for (const letter of verticalJoin) {
+    if (letter.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
